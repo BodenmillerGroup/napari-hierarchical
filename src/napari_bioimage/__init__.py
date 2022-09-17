67645661ac073a5cb8a54a5c8ea4d4fa2909cab4
Jonas Windhager <jonas.windhager@uzh.ch>
2022-09-17 18:59:12 +0200

    controller, sample data

--- src/napari_bioimage/__init__.py
+++ src/napari_bioimage/__init__.py
@@ -3,16 +3,17 @@ try:
 except ImportError:
     __version__ = "unknown"
 
+from ._bioimage_controller import BioImageController
 from ._reader import napari_get_reader
-from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, example_magic_widget
-from ._writer import write_multiple, write_single_image
 
-__all__ = (
+# from ._writer import write_multiple, write_single_image
+
+controller = BioImageController()
+
+__all__ = [
+    "controller",
+    "BioImageController",
     "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+    # "write_single_image",
+    # "write_multiple",
+]
