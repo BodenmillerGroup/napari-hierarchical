592c8eaa5760c217d4e8fbb25b77cf2b2accc88f
Jonas Windhager <jonas.windhager@uzh.ch>
2022-10-09 18:12:40 +0200

    fix IDR sample data s3 endpoint url

--- src/napari_bioimage/sample_data/idr0052A.py
+++ src/napari_bioimage/sample_data/idr0052A.py
@@ -1,12 +1,29 @@
+from contextlib import contextmanager
+
 from napari.viewer import current_viewer
+from s3fs import S3FileSystem
 
 from .._controller import controller
 
-image_url = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0052A/5514375.zarr"
+s3_endpoint_url = "https://uk1s3.embassy.ebi.ac.uk"
+image_url = "s3://idr/zarr/v0.4/idr0052A/5514375.zarr"
+
+
+@contextmanager
+def configure_s3() -> S3FileSystem:
+    s3 = S3FileSystem.current()
+    orig_s3_anon = s3.anon
+    orig_s3_client_kwargs = s3.client_kwargs.copy()
+    s3.anon = True
+    s3.client_kwargs = {"endpoint_url": s3_endpoint_url}
+    yield s3
+    s3.anon = orig_s3_anon
+    s3.client_kwargs = orig_s3_client_kwargs
 
 
 def make_sample_data():
-    controller.read_image(image_url)
+    with configure_s3():
+        controller.read_image(image_url)
     viewer = controller.viewer or current_viewer()
     assert viewer is not None
     if controller.viewer != viewer:
