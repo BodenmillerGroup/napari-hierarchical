672dda490e036a90d9ac1c3dfb9e8e2afe2d493d
Jonas Windhager <jonas.windhager@uzh.ch>
2022-10-21 20:57:53 +0200

    napari legacy reader

--- src/napari_bioimage/__init__.py
+++ src/napari_bioimage/__init__.py
@@ -6,12 +6,14 @@ except ImportError:
 from ._controller import BioImageController, BioImageControllerException, controller
 from ._exceptions import BioImageException
 from ._reader import napari_get_reader
-from .contrib import hdf5, imc, ome_zarr, zarr
+from .contrib import hdf5, imc, napari, ome_zarr, zarr
 
 if hdf5.available:
     controller.pm.register(hdf5, name="napari-bioimage-hdf5")
 if imc.available:
     controller.pm.register(imc, name="napari-bioimage-imc")
+if napari.available:
+    controller.pm.register(napari, name="napari-bioimage-napari")
 if ome_zarr.available:
     controller.pm.register(ome_zarr, name="napari-bioimage-ome-zarr")
 if zarr.available:  # register after napari-bioimage-ome-zarr!
